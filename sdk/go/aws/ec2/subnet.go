3b836b0e94dfea0fa2c8cfda3f745f2251b19d26
Levi Blackstone <levi@pulumi.com>
2021-10-05 10:36:14 -0600

    Update to latest available resources (#155)

--- sdk/go/aws/ec2/subnet.go
+++ sdk/go/aws/ec2/subnet.go
@@ -12,6 +12,8 @@ import (
 )
 
 // Resource Type definition for AWS::EC2::Subnet
+//
+// Deprecated: Subnet is not yet supported by AWS Native, so its creation will currently fail. Please use the classic AWS provider, if possible.
 type Subnet struct {
 	pulumi.CustomResourceState
 
--- sdk/nodejs/ec2/subnet.ts
+++ sdk/nodejs/ec2/subnet.ts
@@ -7,6 +7,8 @@ import * as utilities from "../utilities";
 
 /**
  * Resource Type definition for AWS::EC2::Subnet
+ *
+ * @deprecated Subnet is not yet supported by AWS Native, so its creation will currently fail. Please use the classic AWS provider, if possible.
  */
 export class Subnet extends pulumi.CustomResource {
     /**
@@ -18,6 +20,7 @@ export class Subnet extends pulumi.CustomResource {
      * @param opts Optional settings to control the behavior of the CustomResource.
      */
     public static get(name: string, id: pulumi.Input<pulumi.ID>, opts?: pulumi.CustomResourceOptions): Subnet {
+        pulumi.log.warn("Subnet is deprecated: Subnet is not yet supported by AWS Native, so its creation will currently fail. Please use the classic AWS provider, if possible.")
         return new Subnet(name, undefined as any, { ...opts, id: id });
     }
 
@@ -53,7 +56,9 @@ export class Subnet extends pulumi.CustomResource {
      * @param args The arguments to use to populate this resource's properties.
      * @param opts A bag of options that control this resource's behavior.
      */
+    /** @deprecated Subnet is not yet supported by AWS Native, so its creation will currently fail. Please use the classic AWS provider, if possible. */
     constructor(name: string, args: SubnetArgs, opts?: pulumi.CustomResourceOptions) {
+        pulumi.log.warn("Subnet is deprecated: Subnet is not yet supported by AWS Native, so its creation will currently fail. Please use the classic AWS provider, if possible.")
         let inputs: pulumi.Inputs = {};
         opts = opts || {};
         if (!opts.id) {
