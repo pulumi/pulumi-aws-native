e00b8bd0e9aa5c86705fcae4583fd805c4f6c170
Vivek Lakshmanan <vivek@lakshmanan.ca>
2021-11-03 00:12:41 -0700

    Examples

--- examples/simple-cs/MyStack.cs
+++ examples/simple-cs/MyStack.cs
@@ -2,22 +2,13 @@
 
 using Pulumi;
 using Pulumi.AwsNative.Logs;
-using Pulumi.Random;
 
 class MyStack : Stack
 {
     public MyStack()
     {
-        var name = new RandomString("name", new RandomStringArgs
-        {
-            Length = 8,
-            Special = false,
-            Upper = false
-        });
-
         var logGroup = new LogGroup("log-test", new LogGroupArgs
         {
-            LogGroupName = name.Result,
             RetentionInDays = 90
         });
 
