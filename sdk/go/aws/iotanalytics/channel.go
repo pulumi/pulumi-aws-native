db768cba3ff7d928a79991120b29a82f28e1e585
Levi Blackstone <levi@pulumi.com>
2021-11-03 09:55:31 -0600

    Update to latest available resources

--- sdk/go/aws/iotanalytics/channel.go
+++ sdk/go/aws/iotanalytics/channel.go
@@ -11,8 +11,6 @@ import (
 )
 
 // Resource Type definition for AWS::IoTAnalytics::Channel
-//
-// Deprecated: Channel is not yet supported by AWS Native, so its creation will currently fail. Please use the classic AWS provider, if possible.
 type Channel struct {
 	pulumi.CustomResourceState
 
