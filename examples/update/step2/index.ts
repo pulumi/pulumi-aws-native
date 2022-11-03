3c21e02a1c93aaf39077618f78bf03c787b399e5
Daniel Bradley <daniel@pulumi.com>
2022-11-03 14:55:45 +0000

    Use native IAM role to improve reliability
    Wait issue doesn't happen via CloudControl so can remove artificial delay. We do, however, have to remove the inline policies in step 2 to avoid deletion error: https://github.com/pulumi/pulumi-aws-native/issues/416

--- examples/update/step2/index.ts
+++ examples/update/step2/index.ts
@@ -1,43 +1,43 @@
 // Copyright 2016-2021, Pulumi Corporation.
 
-import * as awsClassic from "@pulumi/aws";
 import * as aws from "@pulumi/aws-native";
 
 const logGroup = new aws.s3.Bucket("bucket", {
-    tags: [{
-        key: "foo",
-        value: "buzz", // <-- this value has changed
-    }]
+  tags: [
+    {
+      key: "foo",
+      value: "buzz", // <-- this value has changed
+    },
+  ],
 });
 
-const lambdaRole = new awsClassic.iam.Role("lambdaRole", {
-    assumeRolePolicy: awsClassic.iam.assumeRolePolicyForPrincipal({ Service: "lambda.amazonaws.com" }),
-    inlinePolicies: [{
-        name: "lambdaAccess", policy: JSON.stringify({
-            Version: "2012-10-17",
-            Statement: [{
-                Effect: "Allow",
-                Action: [
-                    "logs:CreateLogGroup",
-                    "logs:CreateLogStream",
-                    "logs:PutLogEvents",
-                ],
-                Resource: "arn:aws:logs:*:*:*",
-            }],
-        })
-    }]
+const lambdaRole = new aws.iam.Role("lambdaRole", {
+  assumeRolePolicyDocument: {
+    Version: "2012-10-17",
+    Statement: [
+      {
+        Sid: "AllowAssumeRole",
+        Effect: "Allow",
+        Principal: {
+          Service: "lambda.amazonaws.com",
+        },
+        Action: "sts:AssumeRole",
+      },
+    ],
+  },
+  // Remove policies to avoid deletion issue: https://github.com/pulumi/pulumi-aws-native/issues/416
 });
 
 const func = new aws.lambda.Function("function", {
-    runtime: "nodejs16.x",
-    role: lambdaRole.arn,
-    handler: "index.handler",
-    code: {
-        zipFile: `
-exports.handler =  async function(event, context) {
-  console.log("Running")
-  return "bar" // <-- this value has changed
-}
-`,
-    },
+  runtime: "nodejs16.x",
+  role: lambdaRole.arn,
+  handler: "index.handler",
+  code: {
+    zipFile: `
+  exports.handler =  async function(event, context) {
+    console.log("Running")
+    return "bar"  // <-- this value has changed
+  }
+  `,
+  },
 });
