e3925fa2b526d67ba034e769fb9558ad17432224
Matthew Jeffryes <mjeffryes@pulumi.com>
2023-08-03 15:30:01 -0700

    Update simple-ts test to use the new names for WebAcl

--- examples/simple-ts/index.ts
+++ examples/simple-ts/index.ts
@@ -8,28 +8,28 @@ const logGroup = new aws.logs.LogGroup("log-test", {
 
 export const arn = logGroup.arn;
 
-new aws.wafv2.WebACL("acl", { 
-    scope: aws.types.enums.wafv2.WebACLScope.Regional, 
-    defaultAction: { block: {} }, 
-    rules: [{ 
-        name: "AWS-AWSManagedRulesCommonRuleSet", 
-        priority: 1, 
-        statement: { 
-            managedRuleGroupStatement: { 
-                vendorName: "AWS", 
-                name: "AWSManagedRulesCommonRuleSet" 
-            } 
-        }, 
-        overrideAction: { none: {} }, 
-        visibilityConfig: { 
-            sampledRequestsEnabled: true, 
-            cloudWatchMetricsEnabled: true, 
-            metricName: "AWSManagedRulesCommonRuleSet" 
-        } 
-    }], 
-    visibilityConfig: { 
-        cloudWatchMetricsEnabled: true, 
-        metricName: "frontend", 
-        sampledRequestsEnabled: true 
+new aws.wafv2.WebAcl("acl", {
+    scope: aws.types.enums.wafv2.WebAclScope.Regional,
+    defaultAction: { block: {} },
+    rules: [{
+        name: "AWS-AWSManagedRulesCommonRuleSet",
+        priority: 1,
+        statement: {
+            managedRuleGroupStatement: {
+                vendorName: "AWS",
+                name: "AWSManagedRulesCommonRuleSet"
+            }
+        },
+        overrideAction: { none: {} },
+        visibilityConfig: {
+            sampledRequestsEnabled: true,
+            cloudWatchMetricsEnabled: true,
+            metricName: "AWSManagedRulesCommonRuleSet"
+        }
+    }],
+    visibilityConfig: {
+        cloudWatchMetricsEnabled: true,
+        metricName: "frontend",
+        sampledRequestsEnabled: true
     },
 });
