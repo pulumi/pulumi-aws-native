bf418c5a8406d3431ced59fb9805105a9a22da26
Daniel Bradley <daniel@pulumi.com>
2024-02-20 15:15:30 +0000

    Move gen reporting to new file

--- provider/pkg/schema/gen.go
+++ provider/pkg/schema/gen.go
@@ -1369,35 +1369,3 @@ func GatherSemantics(schemaDir string) (SemanticsSpecDocument, error) {
 	}
 	return semanticsDocument, nil
 }
-
-type Reports struct {
-	// UnexpectedTagsShapes is a map of the resource token of resources which have a `Tags` field but don't match an expected pattern.
-	UnexpectedTagsShapes map[string]interface{}
-}
-
-func (r *Reports) WriteToDirectory(dir string) error {
-	err := os.MkdirAll(dir, 0755)
-	if err != nil {
-		return fmt.Errorf("creating directory: %w", err)
-	}
-
-	writeFile := func(filename string, data any) error {
-		f, err := os.Create(filepath.Join(dir, filename))
-		if err != nil {
-			return fmt.Errorf("creating file: %w", err)
-		}
-		defer contract.IgnoreClose(f)
-
-		enc := json.NewEncoder(f)
-		enc.SetIndent("", "  ")
-		if err = enc.Encode(data); err != nil {
-			return fmt.Errorf("writing file: %w", err)
-		}
-		return nil
-	}
-
-	if err = writeFile("unexpectedTagsShapes.json", r.UnexpectedTagsShapes); err != nil {
-		return err
-	}
-	return nil
-}
