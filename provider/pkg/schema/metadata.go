3fbefb4aaace8fbf3cd1afab24ebee1134ab84ce
Daniel Bradley <daniel@pulumi.com>
2024-02-20 16:19:57 +0000

    Revert unused metadata change

--- provider/pkg/schema/metadata.go
+++ provider/pkg/schema/metadata.go
@@ -20,7 +20,6 @@ type CloudAPIResource struct {
 	Required          []string                        `json:"required,omitempty"`
 	CreateOnly        []string                        `json:"createOnly,omitempty"`
 	WriteOnly         []string                        `json:"writeOnly,omitempty"`
-	UnorderedArrays   []string                        `json:"unorderedArrays,omitempty"`
 	IrreversibleNames map[string]string               `json:"irreversibleNames,omitempty"`
 }
 
