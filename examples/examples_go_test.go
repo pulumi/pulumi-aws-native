5ae1b4138b4d86e55c1b5d876e70d451be07b8f1
Daniel Bradley <daniel@pulumi.com>
2024-02-27 16:45:35 +0000

    Re-instate failing test

--- examples/examples_go_test.go
+++ examples/examples_go_test.go
@@ -1,13 +1,16 @@
 // Copyright 2016-2021, Pulumi Corporation.  All rights reserved.
+//go:build go || all
 // +build go all
 
 package examples
 
 import (
+	"fmt"
 	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSimpleGo(t *testing.T) {
@@ -19,12 +22,43 @@ func TestSimpleGo(t *testing.T) {
 	integration.ProgramTest(t, &test)
 }
 
+func TestWriteOnlyGo(t *testing.T) {
+	test := getGoBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			SkipRefresh:          false,
+			ExpectRefreshChanges: false,
+			Dir:                  filepath.Join(getCwd(t), "write-only-go"),
+			Config: map[string]string{
+				"aws-native:defaultTags": `{
+					"defaultTag": "defaultTagValue"
+					}`,
+			},
+			ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+				// We should see both the default tag and the local tag in the log group tags.
+				// If the default tag is missing, it's because there was a missing output from the provider and the SDK filled in the value with the original input.
+				tags := stack.Outputs["tags"].(map[string]interface{})
+				assert.Equal(t, map[string]interface{}{
+					"defaultTag": "defaultTagValue",
+					"localTag":   "localTagValue",
+				}, tags)
+			},
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
 func getGoBaseOptions(t *testing.T) integration.ProgramTestOptions {
 	base := getBaseOptions(t)
+
+	sdkPath, err := filepath.Abs("../sdk")
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	baseGo := base.With(integration.ProgramTestOptions{
 		Verbose: true,
 		Dependencies: []string{
-			"github.com/pulumi/pulumi-aws-native/sdk",
+			fmt.Sprintf("github.com/pulumi/pulumi-aws-native/sdk=%s", sdkPath),
 		},
 	})
 
