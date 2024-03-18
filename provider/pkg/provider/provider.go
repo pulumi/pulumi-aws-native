ec42b13e1bb27773e52cd79d0b4376feade5a493
Daniel Bradley <daniel@pulumi.com>
2024-03-18 15:52:38 +0000

    Client refactor (#1431)
    Sort out dependencies so we can introduce #1406 without introducing
    cycles.
    
    1. Move client to top-level. If it's within provider then we can't
    import it into another module that's going to be imported by provider.
    
    2. Remove resource state conversion in client. This is also done in the
    provider and should not be a concern of the client.
    This also allows us to remove a dependency on pkg/schema.

--- provider/pkg/provider/provider.go
+++ provider/pkg/provider/provider.go
@@ -48,7 +48,7 @@ import (
 	pbempty "github.com/golang/protobuf/ptypes/empty"
 	pbstruct "github.com/golang/protobuf/ptypes/struct"
 	"github.com/pkg/errors"
-	"github.com/pulumi/pulumi-aws-native/provider/pkg/provider/client"
+	"github.com/pulumi/pulumi-aws-native/provider/pkg/client"
 	"github.com/pulumi/pulumi-aws-native/provider/pkg/schema"
 	"github.com/pulumi/pulumi-aws-native/provider/pkg/version"
 	"github.com/pulumi/pulumi-go-provider/resourcex"
@@ -859,6 +859,9 @@ func (p *cfnProvider) Create(ctx context.Context, req *pulumirpc.CreateRequest) 
 	}
 
 	if createErr != nil {
+		if id == nil {
+			return nil, fmt.Errorf("creating resource: %w", createErr)
+		}
 		// Resource was created but failed to fully initialize.
 		// It has some state, so we return a partial error.
 		obj := checkpointObject(inputs, outputs)
