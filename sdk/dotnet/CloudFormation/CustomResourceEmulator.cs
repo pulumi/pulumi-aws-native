2cc99723fed7731d8d058d6c73d8774fa39ad5cb
Florian Stadler <florian@pulumi.com>
2024-11-08 11:19:02 +0100

    Regen SDK

--- sdk/dotnet/CloudFormation/CustomResourceEmulator.cs
+++ sdk/dotnet/CloudFormation/CustomResourceEmulator.cs
@@ -10,7 +10,42 @@ using Pulumi.Serialization;
 namespace Pulumi.AwsNative.CloudFormation
 {
     /// <summary>
-    /// TODO
+    /// The Custom Resource Emulator allows you to use AWS CloudFormation Custom Resources directly in your Pulumi programs. It provides a way to invoke AWS Lambda functions that implement custom provisioning logic following the CloudFormation Custom Resource protocol.
+    /// 
+    /// &gt; **Note**: Currently, only Lambda-backed Custom Resources are supported. SNS-backed Custom Resources are not supported at this time.
+    /// 
+    /// ## Example Usage
+    /// 
+    /// ## About CloudFormation Custom Resources
+    /// 
+    /// CloudFormation Custom Resources allow you to write custom provisioning logic for resources that aren't directly available as AWS CloudFormation resource types. Common use cases include:
+    /// 
+    /// - Managing resources outside of AWS (e.g., GitHub repositories, external APIs)
+    /// - Implementing complex provisioning logic
+    /// - Performing custom validations or transformations
+    /// - Integrating with third-party services
+    /// - Implementing organization-specific infrastructure patterns
+    /// 
+    /// For more information about CloudFormation Custom Resources, see [Custom Resources](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/template-custom-resources.html) in the AWS CloudFormation User Guide.
+    /// 
+    /// ## Permissions
+    /// 
+    /// The IAM principal used by your Pulumi program must have the following permissions:
+    /// 
+    /// 1. `lambda:InvokeFunction` on the Lambda function specified in `serviceToken`
+    /// 2. S3 permissions on the bucket specified in `bucketName`:
+    ///    - `s3:PutObject`
+    ///    - `s3:GetObject`
+    ///    - `s3:HeadObject`
+    /// 
+    /// ## Lambda Function Requirements
+    /// 
+    /// The Lambda function specified in `serviceToken` must implement the CloudFormation Custom Resource lifecycle.
+    /// For detailed information about implementing Lambda-backed Custom Resources, see [AWS Lambda-backed Custom Resources](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/template-custom-resources-lambda.html) in the AWS CloudFormation User Guide.
+    /// 
+    /// ## Timeouts
+    /// 
+    /// Custom Resources have a default timeout of 60 minutes, matching the CloudFormation timeout for custom resource operations. You can customize it using the [`customTimeouts`](https://www.pulumi.com/docs/iac/concepts/options/customtimeouts/) resource option.
     /// </summary>
     [AwsNativeResourceType("aws-native:cloudformation:CustomResourceEmulator")]
     public partial class CustomResourceEmulator : global::Pulumi.CustomResource
