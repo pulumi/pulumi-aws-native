fefca641c6347ac393a0af2598b83c60edf51cfd
Florian Stadler <florian@pulumi.com>
2024-11-08 13:07:23 +0100

    Remove unrelated change

--- provider/pkg/provider/provider.go
+++ provider/pkg/provider/provider.go
@@ -113,12 +113,12 @@ type cfnProvider struct {
 
 	pulumiSchema []byte
 
-	cfn *cloudformation.Client
-	ccc client.CloudControlClient
-	ec2 *ec2.Client
-	ssm *ssm.Client
-	sts *sts.Client
-	s3  *s3.Client
+	cfn    *cloudformation.Client
+	ccc    client.CloudControlClient
+	ec2    *ec2.Client
+	ssm    *ssm.Client
+	sts    *sts.Client
+	s3     *s3.Client
 	lambda *lambda.Client
 
 	customResources map[string]resources.CustomResource
@@ -270,15 +270,14 @@ func (p *cfnProvider) DiffConfig(ctx context.Context, req *pulumirpc.DiffRequest
 		return &pulumirpc.DiffResponse{Changes: pulumirpc.DiffResponse_DIFF_NONE}, nil
 	}
 
-	var diffs, replaces []string
+	var diffs []string
 	for _, k := range diff.Keys() {
 		diffs = append(diffs, string(k))
 	}
 
 	return &pulumirpc.DiffResponse{
-		Changes:  pulumirpc.DiffResponse_DIFF_SOME,
-		Diffs:    diffs,
-		Replaces: replaces,
+		Changes: pulumirpc.DiffResponse_DIFF_SOME,
+		Diffs:   diffs,
 	}, nil
 }
 
