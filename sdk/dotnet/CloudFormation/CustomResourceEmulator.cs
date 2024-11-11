3308fa3d597fc0d93d0b95fa2453e1c5f6bb769e
Florian Stadler <florian@pulumi.com>
2024-11-11 18:00:01 +0100

    Rebuild schema & SDK

--- sdk/dotnet/CloudFormation/CustomResourceEmulator.cs
+++ sdk/dotnet/CloudFormation/CustomResourceEmulator.cs
@@ -16,6 +16,8 @@ namespace Pulumi.AwsNative.CloudFormation
     /// 
     /// ## Example Usage
     /// 
+    /// A full example of creating a CloudFormation Custom Resource Lambda function and using it in Pulumi can be found [here](https://github.com/pulumi/pulumi-aws-native/tree/master/examples/cfn-custom-resource).
+    /// 
     /// ## About CloudFormation Custom Resources
     /// 
     /// CloudFormation Custom Resources allow you to write custom provisioning logic for resources that aren't directly available as AWS CloudFormation resource types. Common use cases include:
