2d3857af914c14f9440783d7acec60e3382387a3
Alberto Pose <albertopose@gmail.com>
2025-10-09 10:41:29 +0100

    Add test and improve code organization.

---
+++ provider/pkg/metadata/aws_marketplace_identifier.go
@@ -0,0 +1,6 @@
+// Copyright 2016-2021, Pulumi Corporation.
+
+package metadata
+
+// The APN 1.1 AWS Marketplace identifier to should be used in the User-Agent header.
+var PulumiAWSMarketplaceCode string = "c7qiae2l6usvzoynupds6v7hf"
