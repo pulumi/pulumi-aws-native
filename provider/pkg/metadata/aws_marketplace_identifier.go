b81375a7c6648299f4195e8ead2641f9cdce99b5
Alberto Pose <albertopose@gmail.com>
2025-10-10 16:15:35 +0100

    Addressing PR feedback.

--- provider/pkg/metadata/aws_marketplace_identifier.go
+++
@@ -1,6 +0,0 @@
-// Copyright 2016-2021, Pulumi Corporation.
-
-package metadata
-
-// The APN 1.1 AWS Marketplace identifier to should be used in the User-Agent header.
-var PulumiAWSMarketplaceCode string = "c7qiae2l6usvzoynupds6v7hf"
